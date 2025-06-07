62ee1499df38b160dd4291274d321a513f575813
jonahgtibbs <120422895+jonahgtibbs@users.noreply.github.com>
2025-06-07 00:38:52 -0500

    Update CaelumPage.tsx

--- src/components/CaelumPage.tsx
+++ src/components/CaelumPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Hero } from './Hero';
-import { About } from './About';
+import { CaelumHero } from './CaelumHero';
+import { CaelumAbout } from './CaelumAbout';
 
 export const CaelumPage: React.FC = () => {
   return (
     <div className="px-6 pb-12">
-      <Hero />
-      <About />
+      <CaelumHero />
+      <CaelumAbout />
       <div className="max-w-4xl mx-auto text-center mt-12">
         <img
           src={`${window.location.origin}/images/Caelum.png`}
