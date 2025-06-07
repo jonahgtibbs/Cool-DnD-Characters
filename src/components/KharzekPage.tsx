81b1115010e2be76c3096df01f10192b8f1457a7
jonahgtibbs <120422895+jonahgtibbs@users.noreply.github.com>
2025-06-07 00:31:54 -0500

    Update KharzekPage.tsx

--- src/components/KharzekPage.tsx
+++ src/components/KharzekPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Hero } from './Hero';
+import { KharzekHero } from './KharzekHero';
 import { About } from './About';
 
 export const KharzekPage: React.FC = () => {
