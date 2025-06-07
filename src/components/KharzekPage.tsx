9f4934d2c3bca691cdb649268231be6cd80a355f
jonahgtibbs <120422895+jonahgtibbs@users.noreply.github.com>
2025-06-07 00:33:33 -0500

    Update KharzekPage.tsx

--- src/components/KharzekPage.tsx
+++ src/components/KharzekPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { KharzekHero } from './KharzekHero';
-import { About } from './About';
+import { KharzekAbout } from './KharzekAbout';
 
 export const KharzekPage: React.FC = () => {
   return (
     <div className="px-6 pb-12">
-      <Hero />
-      <About />
+      <KharzekHero />
+      <KharzekAbout />
       <div className="max-w-4xl mx-auto text-center mt-12">
         <img
           src={`${window.location.origin}/images/Kharzek.png`}
